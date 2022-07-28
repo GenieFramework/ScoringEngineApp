fddae7db6ba6bb08a5cfa2316c4a3db5cd734ef0
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2022-07-28 14:24:50 +0530

    added packagecompiler support, dockerfile and cdn support

--- routes.jl
+++ routes.jl
@@ -8,6 +8,9 @@ using Stipple.ModelStorage.Sessions
 using ScoringEngineApp
 using ScoringEngineApp.ScoringEngine
 
+if Genie.Configuration.isprod()
+  Genie.Assets.assets_config!([Genie, Stipple, StippleUI, StipplePlotly], host = "https://cdn.statically.io/gh/GenieFramework")
+end
 
 Page("/", view = "views/scoreboard.jl.html",
           layout = "layouts/app.jl.html",
